e0b160c249a55d09aa4082a72df4659bc121e44b
Ibrahemherzallah <ibrahemherzallah@gmail.com>
2025-09-13 19:00:35 +0300

    fix iphone mobile view issue

--- frontend/src/pages/admin/modals/addWholesalers.jsx
+++ frontend/src/pages/admin/modals/addWholesalers.jsx
@@ -59,7 +59,7 @@ export const AddWholesalers = ({product,isUpdated}) => {
             <div className="modal-dialog modal-dialog-centered">
                 <div className={`modal-content ${isDark ? "bg-dark text-white" : "" }`}>
                     <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="exampleModalLabel">{isUpdated ? 'Update Wholesaler' : 'Add Wholesaler'}</h1>
+                        <h1 className="modal-title fs-5" id="exampleModal5Label">{isUpdated ? 'Update Wholesaler' : 'Add Wholesaler'}</h1>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <form onSubmit={handleOnSubmit}>
